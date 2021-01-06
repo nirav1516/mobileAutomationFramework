7d23473e8f2509a10e9afbdc66654826b669ec5b
Nirav Shah <nirav1516@gmail.com>
2021-01-06 01:14:44 -0800

    Creating maven projects with dependancies in pom.xml

---
+++ AppiumFramework/src/main/java/mypractice/AppiumFramework/App.java
@@ -0,0 +1,13 @@
+package mypractice.AppiumFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
